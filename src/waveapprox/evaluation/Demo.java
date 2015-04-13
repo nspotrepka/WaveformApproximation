f98e81c94a29b96d11ea9e4a8d78559707f49936
sa- <samay.kapadia@gmail.com>
2015-04-13 12:17:16 -0400

    Moved eval files around

---
+++ src/waveapprox/evaluation/Demo.java
@@ -0,0 +1,10 @@
+package waveapprox.evaluation;
+
+public class Demo {
+	public static void main(String[] args){
+		//a should be 0
+		int a = WaveFileReader.WavDifference("wav/wav_20_8_1_pcm.wav","wav/wav_20_8_1_pcm.wav");
+		int b = WaveFileReader.WavDifference("wav/wav_40_16_1_pcm.wav","wav/wav_20_8_2_pcm.wav");
+		System.out.println(a + "\n" + b);
+	}
+}
